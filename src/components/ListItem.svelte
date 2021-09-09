889910a096c5577e99b8aa0bbfa4725aea3b9d52
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-09-09 08:43:57 -0500

    add ability to check off items from list

--- src/components/ListItem.svelte
+++ src/components/ListItem.svelte
@@ -1,7 +1,13 @@
 <script>
   export let item;
+
+  const handleItemClick = (e) => {
+    const elt = e.target;
+    elt.classList.toggle("crossed-line");
+    console.log(elt.classList);
+  };
 </script>
 
-<div>
+<div on:click={handleItemClick}>
   {item.label}
 </div>
